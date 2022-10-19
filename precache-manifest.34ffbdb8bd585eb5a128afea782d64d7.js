1af46da5158ae5ba22e4c3d298ea65569425e287
AlexGNovo <alex_guno0110@hotmail.com>
2022-10-19 18:49:04 -0500

    Updates

---
+++ precache-manifest.34ffbdb8bd585eb5a128afea782d64d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36b55d73b9ed8d77f62b",
+    "url": "/Cal-App/static/css/main.4280fa3c.chunk.css"
+  },
+  {
+    "revision": "36b55d73b9ed8d77f62b",
+    "url": "/Cal-App/static/js/main.98935413.chunk.js"
+  },
+  {
+    "revision": "83a7000d9d45e08ba9cb",
+    "url": "/Cal-App/static/js/runtime~main.8b6b193e.js"
+  },
+  {
+    "revision": "b4bb6a66da2c4b72497c",
+    "url": "/Cal-App/static/js/2.a24b5087.chunk.js"
+  },
+  {
+    "revision": "6d63152473e4e8715c1c4327ad973bbd",
+    "url": "/Cal-App/index.html"
+  }
+];
